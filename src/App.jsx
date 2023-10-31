f6c3526c5ce1d8fee393a29d8b3ad35f6f5be711
kingsamuel Chinweikpe Nwachukwu <kingsamuelronaldnwachukwu@gmail.com>
2023-10-31 17:54:53 +0100

    improved the view for mobile device

--- src/App.jsx
+++ src/App.jsx
@@ -33,34 +33,34 @@ export default function CalcBuild(){
     <>
       <div className="grid grid-cols-4 gap-4 p-1 bg-black m-20 rounded-lg w-100 p-2">
 
-        <button class="text-white bg-[#616161] rounded-lg p-2 font-bold hover:bg-[#5dcc89]" onClick={() => handleButtonClick('C')} >C</button>
-          <button class="text-white bg-[#616161] rounded-lg font-bold hover:bg-[#5dcc89]" onClick={() => handleButtonClick('(')}  >(</button>
-          <button class="text-white bg-[#616161] rounded-lg font-bold hover:bg-[#5dcc89]" onClick={() => handleButtonClick(')')} >)</button>
-          <button class="text-white bg-[#077634] rounded-lg font-bold hover:bg-[#5dcc89]"  onClick={() => handleButtonClick('*')}>*</button>
-          <button class="text-white bg-[#303136] rounded-lg p-2 text-[#02a543] font-bold hover:bg-[#616161]" onClick={() => handleButtonClick('7')} >7</button>
-          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161] " onClick={() => handleButtonClick('8')}>8</button>
-          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161]" onClick={() => handleButtonClick('9')}>9</button>
-          <button class="text-white bg-[#077634] rounded-lg font-bold hover:bg-[#5dcc89]" onClick={() => handleButtonClick('-')}>-</button>
-          <button class="text-white bg-[#303136] rounded-lg p-2 font-bold text-[#02a543] hover:bg-[#616161]" onClick={() => handleButtonClick('4')}>4</button>
-          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161]" onClick={() => handleButtonClick('5')}>5</button>
-          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161]" onClick={() => handleButtonClick('5')}>6</button>
-          <button class="text-white bg-[#077634] rounded-lg font-bold hover:bg-[#5dcc89]" onClick={() => handleButtonClick('+')}>+</button>
-          <button class="text-white bg-[#303136] rounded-lg p-2 font-bold text-[#02a543] hover:bg-[#616161]" onClick={() => handleButtonClick('1')}>1</button>
-          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161]" onClick={() => handleButtonClick('2')}>2</button>
-          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161]" onClick={() => handleButtonClick('3')}>3</button>
-          <button class="text-white bg-[#077634] rounded-lg font-bold hover:bg-[#5dcc89]" onClick={() => handleButtonClick('/')} >/</button>
-          <button class="text-white bg-[#303136] rounded-lg p-2 font-bold text-[#02a543] hover:bg-[#616161]" onClick={() => handleButtonClick('0')}>0</button>
-          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161]" onClick={() => handleButtonClick('.')}>.</button>
-          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161]" onClick={() => handleButtonClick('%')}>%</button>
-          <button class="text-white bg-[#077634] rounded-lg font-bold hover:bg-[#5dcc89]" onClick={() => handleButtonClick('=')}>=</button>
+        <button class="text-white bg-[#616161] rounded-lg p-2 font-bold hover:bg-[#5dcc89] text-[30px]" onClick={() => handleButtonClick('C')} >C</button>
+          <button class="text-white bg-[#616161] rounded-lg font-bold hover:bg-[#5dcc89 text-[30px]" onClick={() => handleButtonClick('(')}  >(</button>
+          <button class="text-white bg-[#616161] rounded-lg font-bold hover:bg-[#5dcc89] text-[30px]" onClick={() => handleButtonClick(')')} >)</button>
+          <button class="text-white bg-[#077634] rounded-lg font-bold hover:bg-[#5dcc89] text-[30px]"  onClick={() => handleButtonClick('*')}>*</button>
+          <button class="text-white bg-[#303136] rounded-lg p-2 text-[#02a543] font-bold hover:bg-[#616161] text-[30px]" onClick={() => handleButtonClick('7')} >7</button>
+          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161] text-[30px]" onClick={() => handleButtonClick('8')}>8</button>
+          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161] text-[30px]" onClick={() => handleButtonClick('9')}>9</button>
+          <button class="text-white bg-[#077634] rounded-lg font-bold hover:bg-[#5dcc89] text-[30px]" onClick={() => handleButtonClick('-')}>-</button>
+          <button class="text-white bg-[#303136] rounded-lg p-2 font-bold text-[#02a543] hover:bg-[#616161] text-[30px]" onClick={() => handleButtonClick('4')}>4</button>
+          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161] text-[30px]" onClick={() => handleButtonClick('5')}>5</button>
+          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161] text-[30px]" onClick={() => handleButtonClick('5')}>6</button>
+          <button class="text-white bg-[#077634] rounded-lg font-bold hover:bg-[#5dcc89] text-[30px]" onClick={() => handleButtonClick('+')}>+</button>
+          <button class="text-white bg-[#303136] rounded-lg p-2 font-bold text-[#02a543] hover:bg-[#616161] text-[30px]" onClick={() => handleButtonClick('1')}>1</button>
+          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161] text-[30px]" onClick={() => handleButtonClick('2')}>2</button>
+          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161] text-[30px]" onClick={() => handleButtonClick('3')}>3</button>
+          <button class="text-white bg-[#077634] rounded-lg font-bold hover:bg-[#5dcc89] text-[30px]" onClick={() => handleButtonClick('/')} >/</button>
+          <button class="text-white bg-[#303136] rounded-lg p-2 font-bold text-[#02a543] hover:bg-[#616161] text-[30px]" onClick={() => handleButtonClick('0')}>0</button>
+          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161] text-[30px]" onClick={() => handleButtonClick('.')}>.</button>
+          <button class="text-white bg-[#303136] rounded-lg font-bold text-[#02a543] hover:bg-[#616161] text-[30px]" onClick={() => handleButtonClick('%')}>%</button>
+          <button class="text-white bg-[#077634] rounded-lg font-bold hover:bg-[#5dcc89] text-[30px]" onClick={() => handleButtonClick('=')}>=</button>
       </div>
 
-      <div class="bg-white p-5 text-center">
+      <div class="bg-white p-5 text-center font-bold text-[30px]">
          {result || currentInput}
       </div>
 
-      <div class="font-bold text-center m-8">
-        Built/Designed  By Kingsamuel Chinweikpe Nwachukwu..
+      <div class="font-bold text-center m-8 text-[30px]">
+      Calculator  Built/Designed  By Kingsamuel Chinweikpe Nwachukwu..
       </div>
     </>
   );
